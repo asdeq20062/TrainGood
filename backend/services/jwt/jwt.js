ddce166a6bf7d1a45de10888ac15cae3804df7ea
EdwardTsai <edward@EdwardTsai.local>
2021-04-26 21:48:26 +0800

    Add member's detail function

--- backend/services/jwt/jwt.js
+++ backend/services/jwt/jwt.js
@@ -7,6 +7,7 @@ const secret = process.env['JWT_SERCET'] || 'gymisgood';
 
 /*
     @param {Object} payload
+    @param {int} payload.id
     @param {string} payload.username
     @param {string} payload.pw
 */
